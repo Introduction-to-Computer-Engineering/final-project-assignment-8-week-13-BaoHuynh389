9835fc4fea8f62e0b23c23a881e01ec8583c7296
BaoHuynh389 <54543807+BaoHuynh389@users.noreply.github.com>
2019-11-24 14:16:08 -0700

    Create servo.js

---
+++ servo.js
@@ -0,0 +1,11 @@
+//code that sets length of PWM pulses
+//a small program to demo the operation of other servo function
+basic.forever(function () {//runs continuously in a loop
+
+    for (let i = 9; i <= 500; i += 10) {//increments i by ten
+        pins.servoWritePin(AnalogPin.P1, 180);//sends a pulse
+        basic.pause(500)//pauses so the user can see what is happening
+        pins.servoSetPulse(AnalogPin.P1, i);//sets duration of pulse
+        basic.pause(500);//pauses so user can see what is happening
+    }
+})
