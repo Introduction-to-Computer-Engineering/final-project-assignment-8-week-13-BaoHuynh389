9dbc2ac04eb4edb4a32d6a2e00b473f59a4cb93f
BaoHuynh389 <54543807+BaoHuynh389@users.noreply.github.com>
2019-11-24 14:21:06 -0700

    Update PWM pulses.js

--- PWM pulses.js
+++ PWM pulses.js
@@ -1,3 +1,3 @@
 //a one-line micro:bit program to set an analog pin to emit PWM pulses
-//PIN 1 is used to
-pins.servoSetPulse(AnalogPin.P1, 1500)
+//PIN 0 is used to
+pins.servoSetPulse(AnalogPin.P0, 1500)
